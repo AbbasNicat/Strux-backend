b337fb6e79dafeb676daea69a147cb1785941786
AbbasNicat <abbasnicat55@gmail.com>
2025-12-19 05:54:42 +0400

    security configs update

--- project-service/project-service/src/main/java/com/strux/project_service/config/SecurityConfig.java
+++ project-service/project-service/src/main/java/com/strux/project_service/config/SecurityConfig.java
@@ -32,8 +32,14 @@ public class SecurityConfig {
                 "http://localhost:5173",
                 "http://localhost:5174",
                 "http://localhost:3000",
-                "http://localhost:5175"
+                "http://localhost:5175",
+                "https://struxtech.app",
+                "https://www.struxtech.app",
+                "https://admin.struxtech.app",
+                "https://app.strux.az",
+                "https://api.struxtech.app"
         ));
+
 
         configuration.setAllowedMethods(Arrays.asList(
                 "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"
